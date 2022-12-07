5eb8cbbeb543932b32e5e801e423b0c885635186
Piotr <pmagier99@gmail.com>
2022-12-07 10:42:41 +0000

    Creating Variable system part 2

---
+++ src/Variable.java
@@ -0,0 +1,20 @@
+
+public class Variable {
+
+    String name;
+    String value;
+
+    public Variable(String name, String value){
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getValue(){
+        return value;
+    }
+
+    public void setValue(String value){
+        this.value = value;
+    }
+
+}
